e62d5f31d9e695b0aab27fcb77d9ea2bcd1c9d3f
robykartis <robykarti.s@gmail.com>
2023-08-07 11:22:17 +0700

    Port

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">5 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">React (Next.js) and PHP (Laravel)</span>.
       </motion.h1>
 
       <motion.div
