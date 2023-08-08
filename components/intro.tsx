5657a4d6f2ac889e34c008228670ee0f4d36516f
Roby Karti S S.KOM <Robykarti.s@gmail.com>
2023-08-08 22:17:16 +0700

    Likending Update Profil

--- components/intro.tsx
+++ components/intro.tsx
@@ -101,7 +101,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/roby-karti-s-271516191/"
+          href="https://www.linkedin.com/in/robykartis/"
           target="_blank"
         >
           <BsLinkedin />
@@ -121,7 +121,6 @@ export default function Intro() {
         >
           <AiFillInstagram />
         </a>
-
       </motion.div>
     </section>
   );
